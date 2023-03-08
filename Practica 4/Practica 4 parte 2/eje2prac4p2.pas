d59e007d830f7517ee405759eadb20d4ebbdf742
Gianluca Cardone <114377978+Giancardonee@users.noreply.github.com>
2023-03-08 15:07:32 -0300

    Add files via upload

--- Practica 4/Practica 4 parte 2/eje2prac4p2.pas
+++ Practica 4/Practica 4 parte 2/eje2prac4p2.pas
@@ -1,160 +1,177 @@
-{2. Realice un programa que resuelva los siguientes incisos:
+{Realice un programa que resuelva los siguientes incisos:
 a. Lea nombres de alumnos y los almacene en un vector de a lo sumo 500 elementos. La lectura finaliza
-cuando se lee el nombre ‘ZZZ’, que no debe procesarse.}
+cuando se lee el nombre ‘ZZZ’, que no debe procesarse.
+b. Lea un nombre y elimine la primera ocurrencia de dicho nombre en el vector.
+c. Lea un nombre y lo inserte en la posición 4 del vector.
+d. Lea un nombre y lo agregue al vector.
+Nota: Realizar todas las validaciones necesarias.}
 
-program eje2_Prac4p2 ; 
+program eje2prac4p2; 
 
-const 
-	DimF = 500 ; 
-	
-type 
-	
-	vector = array [1..DimF] of string ; 
+Const 
+  DimF = 500 ; 
 
-procedure saltodeLinea () ; 
-begin
-	writeln ('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -') ; 
-end; 
-	
-procedure CargarVector (var v: vector ; DimF: integer ; var DimL: integer) ; 
+type
+	vector = array [1..DimF] of string;
+	 
+procedure cargarVectorNombres(var v:vector; var DimL: integer); 
 var
 nombre: string; 
 begin
 DimL:= 0 ; 
-	write ('Ingrese un nombre: ');  readln (nombre) ; 
-		while ( (DimL <= DimF) and (nombre <> 'ZZZ' ) ) do 
-			begin	
-			DimL:= DimL + 1 ; 
-			v[DimL]:= nombre; 
-			write ('Ingrese un nombre: ');  readln (nombre) ; 
-			end; 
-end; 
-{b. Lea un nombre y elimine la primera ocurrencia de dicho nombre en el vector.}
-procedure BorrarElemento (var v: vector; var DimL : integer ); 
-var
-Posicion, i : integer;
-pude: boolean ; 
-begin
-Posicion:= 1 ; 
-pude:= false; 
-	// como nos pide realizar todas las validaciones necesarias, vamos a hacerlo preguntado y validando todo
-if  (Posicion >= 1 )  and  (Posicion <= DimL )  then
-	begin
-		for i:= Posicion to ( DimL - 1 ) do 
-			v[i] := v[i + 1 ] ; 
-	
-			pude:= true; 
-			DimL:= (DimL - 1) 
-	end; 
-
-if( pude = false)  then 
-writeln ('No se puede eliminar la primer posicion del vector') ; 
-
-end;
-
-procedure MostrarElementosVector (v: vector; DimL : integer) ; 
-var
-i: integer; 
-begin
-	for i:= 1 to DimL do 
+nombre:= ' '; 
+	while nombre <> 'ZZZ' do 
 		begin
-			write ('En la posicion ',i,' se cargo el nombre ',v[i]) ; 
-					writeln ; 
-		end; 
-end;
-
-// Lea un nombre y lo inserte en la posición 4 del vector.
-procedure insertarPos (var v: vector; DimL: integer) ; 
-var
-pos: integer; 
-nombre: string; 
-pude: boolean ; 
-i: integer; 
-begin
-pude:= false; 
-pos:= 4; 
-
-	if DimL >= 4 then 
-		begin
-			write ('Ingrese un nombre para insertar en el vector en la posicion 4: ') ; readln (nombre) ;  
-
-					saltodeLinea ; 
-
-
-			if (DimL + 1 <= DimF) and (pos <= DimL) and (pos >= 1) then begin 
-					for i:= DimL downto pos do 
-							v[DimL + 1 ]:= v[DimL]; 
-									pude:= true; 	
-											v[pos]:= nombre; 
-													DimL:= DimL + 1 ; 		
-		end; 
-		
-				if (pude = false) then 
-							writeln ('No se puede insertar el nombre en la pos ',pos) ; 
-		end; 
-
-end; 
-
-
-procedure AgregarElemento (var v: vector ; var DimL : integer ) ; 
-var
-pude: boolean ; 
-nombre: string;  
-begin
-	pude:= false; 
-		SaltoDeLinea ;
-						write ('Ingresa un nombre para agregar al vector : ') ; readln (nombre); 
-				if ( (DimL + 1) <= DimF ) then 
+				write('Ingrese un nombre: '); 
+					readln(nombre); 
+						if nombre <> 'ZZZ' then 
 							begin
-									pude:= true ; 
-										DimL := DimL + 1 ; 
-												v[DimL]:= nombre; 
-							 end;
-	  
-	  SaltoDeLinea ; 
-				if (pude = true )then begin 
-							writeln ('El vector con el nombre ',nombre, ' en la posicion ',DimL, ' quedaria asi: ' ) ; 
-						writeln  
-					end
-						else
-							writeln ('No se pudo agregar el nombre, porque no hay mas lugar en el vector '); 
- 
-end ; 
+							DimL:= DimL + 1 ; 
+						        v[DimL]:= nombre;  
+							end; 
+		end; 
+end; 
+//b) Leer nombre y eliminar ocurrencia 
+procedure eliminarOcurrencia (var v:vector; var DimL: integer); 
+var
+nombre:string; 
+pos,i: integer; 
+encontre: boolean ; 
 
-// prog princ	
-var 
-v: vector ;
-DimL : integer;
 begin
-CargarVector (v,DimF,DimL) ; 
+pos:=1; 
+encontre:= false; 
+	write('Ingrese un nombre para eliminar: ') ; readln (nombre); 
+		//Buscamos si el nombre se encuentra en el vector
+			while ((pos<= DimL) and (not encontre)) do 
+				begin
+						if nombre = v[pos] then 
+							encontre:= true 
+						  else
+								pos:= pos + 1 ; 
+				end; 
+			// si encontre = true es porque se encontro el nombre
+			// y en pos, nos va a indicar la posicion que se encontro
+			//por lo tanto: debemos eliminar el nombre 
+			
+	if (encontre = true) then 
+	//elimino ocurrencia
+		begin
+			for i:= pos to (DimL - 1)do  
+					begin
+					v[i]:= v[i+1] 
+					end; // end del for 
+				DimL:= DimL - 1 ; 
+				writeln ('Se elimino el nombre correctamente'); 
+	 	end; // end del if  
+    	
+    	if (encontre = false) then 
+			writeln ('**ERROR** el nombre no se encuentra en el vector') ;	
+end; 
+
+//c. Lea un nombre y lo inserte en la posición 4 del vector.
+procedure insertarNombre (var v:vector; var DimL:integer); 
+var
+nombre:string; 
+pos: integer; 
+pude:boolean; 
+i: integer; 
+begin
+{para saber si se puede insertar el nombre en la posicion 4
+* debemos saber si hay espacio (DimL + 1 tiene que ser <o= que DimF )
+* y debemos verificar que la posicion sea valida }
+{En este caso, sabemos la posicion,pero no sabemos la dimension logica del vector}
+pos:=4 ; 
+pude:= false; 
+	if ((DimL + 1) <= (DimF))  and (pos>=1) and (pos<=DimL) then 
+		begin
+			pude:= true; 
+				write('Ingrese un nombre para insertar en la posicion 4: '); 
+					readln (nombre); 
+							for i:= DimL downto pos do 
+								v[i+1]:= v[i]; 
+								DimL:= DimL + 1 ; 
+								v[pos]:= nombre; 
+		end; 
+		if (pude = true) then 
+			writeln('El nombre ',nombre,' se inserto correctamente en la posicion 4')
+			else
+				writeln('**ERROR** no se pudo insertar el nombre. '); 
+		
+end; 
+
+//d. Lea un nombre y lo agregue al vector.
+procedure agregarNombreVector (var v:vector; var DimL:integer) ; 
+var
+nombre:string; 
+pude: boolean; 
+begin
+{para agregar un elemento al final del vector, primero debemos
+* saber si DimL<= DimF, en el caso de que el vector no tenga
+* mas espacio, nos va a devolver false
+* Si el vector tiene espacio, nos devuelve true, y podemos agregar el nombre}
+pude:= false; 
+	if ((DimL+1) <= DimF) then 
+		begin
+		pude:= true; 
+			write ('Ingrese un nombre para agregar al final del vector: '); 
+				readln (nombre); 
+					DimL:= DimL+1; 
+					v[DimL]:= nombre; 
+		end;
+		if (pude = true) then 
+			writeln ('Se agrego correctamente el nombre ingresado al final del vector')
+		else
+			begin
+				writeln ('**ERROR** no se pudo ingresar el nombre ingresado') ;
+				writeln('el vector se encuentra lleno')
+			end; 
+
+end; 
 
 
-saltodeLinea ; 
-	
-// muestro el vector con todos los elementos cargados 
-writeln ('El vector con todos los elementos cargados queda asi: '); 
-writeln ; 
-MostrarElementosVector (v,DimL); 
+procedure MostrarVector (v:vector; DimL:integer) ; 
+var
+i: integer; 
+begin
+	for i := 1 to DimL do 
+		writeln (v[i]); 
+end;
 
-saltodeLinea; 
+procedure separar ; 
+begin
+	writeln (' - - - - - - - - - - - - - - - - - - - - ')
+end; 
 
-//inciso b: Borrar primera posicion del vector 
-BorrarElemento (v,DimL) ; 
-writeln  ('El vector con la primera ocurrencia eliminada queda asi:  '  ); 
-writeln; 
-MostrarElementosVector (v,DimL); 
+// prog. princ
+var
+v:vector; 
+DimL,opcion: integer; 
+begin
+opcion:= -1 ; 
+cargarVectorNombres(v,DimL);
+while (opcion <> 0) do 
+	begin 
+	    separar; 
+	   writeln('Operaciones que se podran realizar sobre el vector: '); 
+		writeln ('(1) Eliminar nombre del vector'); 
+		writeln ('(2) Insertar nombre en el vector');
+		writeln('(3) Agregar nombre en el vector');  
+		writeln('(4) Mostrar vector'); 
+		writeln('(0) Para salir'); 
+		write ('Ingrese una opcion: ');
+			readln (opcion);  
+			case opcion of 
+			1:eliminarOcurrencia(v,DimL);  
+			2:insertarNombre(v,DimL); 
+		    3: agregarNombreVector(v,DimL); 
+			4:MostrarVector(v,DimL); 
+			
+			
+			end; // end del case 
+			separar; 
+	end; // end del while 
 
-saltodeLinea ;
+  
 
-//inciso c : Lea un nombre y lo inserte en la posición 4 del vector.
-insertarPos (v, DimL); 
-writeln ('El vector con el nombre ingresado, insertado en la posicion 4 queda asi: '); 
-writeln ; 
-MostrarElementosVector (v,DimL); 
-
-// inciso d : Lea un nombre y lo agregue al vector. 
-writeln ('El vector con un nombre leido por teclado, y agregado al vector queda asi: ') ; 
-AgregarElemento (v,DimL) ; 
-MostrarElementosVector (v,DimL) ; 
-SaltoDeLinea; 
-end. 	
+end.
