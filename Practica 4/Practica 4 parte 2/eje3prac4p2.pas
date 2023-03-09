5705188c0a0863da1c2522d1b626edbc2895c581
Gianluca Cardone <114377978+Giancardonee@users.noreply.github.com>
2023-03-08 23:09:28 -0300

    Add files via upload

--- Practica 4/Practica 4 parte 2/eje3prac4p2.pas
+++ Practica 4/Practica 4 parte 2/eje3prac4p2.pas
@@ -1,4 +1,4 @@
-{3. Una empresa de transporte de caudales desea optimizar el servicio que brinda a sus clientes. Para ello,
+{  Una empresa de transporte de caudales desea optimizar el servicio que brinda a sus clientes. Para ello,
 cuenta con información sobre todos los viajes realizados durante el mes de marzo. De cada viaje se cuenta
 con la siguiente información: día del mes (de 1 a 31), monto de dinero transportado y distancia recorrida
 por el camión (medida en kilómetros).
@@ -13,198 +13,120 @@ recorrida sea igual a 100 km.
 Nota: para realizar el inciso b, el vector debe recorrerse una única vez.}
 
 program eje3prac4p2; 
-		
-type 
-			rangoDias = 1..31; 	
-			RangoVector = 1..200 ; 
-///--------------------------------------------------
-		 DatosViajes = record 
-		
-		 Dia: integer; 
-		DineroTransportado: real ; 
-		Km : real ; 
-		end; 
-//----------------------------------------------------
-	vDias = array [rangoDias] of integer; 
 
-	vViajes = array [RangoVector] of DatosViajes; 
-//------------------------------------------------------
+Const
+	DimF = 200; 
 
-//-----------------------------------------------------
-//inicializo vector de dias [1..31], en 0 cada posicion 
-procedure inicializo_cero (var v: vDias) ; 
-var
-i: integer; 
-begin
-	for i := 1 to 31 do
-		begin
-					v[i]:= 0 ; 
-		end; 
-end;
+Type 
+	viaje = record
+		Dia: integer; 
+		dineroTransportado: real; 
+		Km: real; 
+	end; 
+	vector = array [1..DimF] of viaje; 
+	vectorDias = array [1..31] of integer; 
 
-{a. Realizar un programa que lea y almacene la información de los viajes (a lo sumo 200). La lectura finaliza
-cuando se ingresa una distancia recorrida igual a 0 km, que no debe procesarse.}
-procedure LeerViajes (var v1: vViajes; var v2: vDias; var DimL : integer; var montoPromedio: real) ; 
-var
-km: real; 
-dia: integer; 
-begin
-DimL:= 0 ; 
-montoPromedio:= 0 ; 
-	write ('Ingrese los km recorridos: '); readln (km); 
-				if km <> 0 then 
-						begin
-								DimL:= 1; 
-		// guardo la cantidad de km en la posicion 1 : 
-						v1[DimL].Km:= km; 
-									write ('Ingrese el dinero transportado: $') ; readln (v1[DimL].DineroTransportado);
-									
-									montoPromedio:= montoPromedio + v1[DimL].DineroTransportado; 
-										
-								// aca debo tener en cuenta que segun el dia que ingresen, tendria que ir incrementando  
-							write ('Ingrese el dia de MARZO que se realizo el viaje:  ');  readln (v1[DimL].Dia);
-											dia:= v1[DimL].Dia ; 
-									v2[dia]:= v2[dia] + 1; 
-									
-											writeln; 
-				end;
-							while( DimL >=1 ) and (DimL <= 200) and (km<> 0)  do 
-								begin
-												write ('Ingrese los km recorridos: ') ; readln (km) ; 		
-													if km <> 0 then 
-														begin
-																	DimL:= DimL + 1 ; 
-																			v1[DimL].Km:= km ; 
-																					write ('Ingrese el dinero transportado: $') ; readln (v1[DimL].DineroTransportado); 
-									
-																							montoPromedio:= montoPromedio + v1[DimL].DineroTransportado; 
-									
-									// aca debo tener en cuenta que segun el dia que ingresen, tendria que ir incrementando  
-														write ('Ingrese el dia de MARZO que se realizo el viaje:  ');  readln (v1[DimL].Dia );  
-																			dia:= v1[DimL].Dia ; 
-																			v2[dia]:= v2[dia] + 1; 
-															writeln ; 
-														end; // end del if  
-								end;    // end del while 
-end;  // end del procedure 
-
-procedure MostrarVectorDias (dias: vDias) ; 
+procedure inicializarVectorDia (var v2:vectorDias) ; 
 var
 i: integer;
 begin
-	for i := 1 to 31 do 
+	for i:= 1 to 31 do 
+		v2[i]:= 0 ; 
+end; 
+	
+{ a. Realizar un programa que lea y almacene 
+la información de los viajes (a lo sumo 200)} 
+procedure cargarDatos (var v:vector; var DimL: integer) ;
+var 
+DistanciaKm: integer ; 
+begin
+	DimL:= 0 ;
+	 DistanciaKm:= -1 ; 
+	while ((DistanciaKm <> 0) and (DimL<= DimF)) do 
 		begin
-		write ('-------> ') ; 
-		write ('En el dia ',i:2, ' se hicieron: ',dias[i]:2, ' viajes ');
-			writeln ; 
+				write ('Ingrese la distancia recorrida: '); 
+					readln (DistanciaKm); 
+						if (DistanciaKm <>0 ) then 
+							begin
+									DimL:= DimL + 1 ; 
+									v[DimL].Km:= DistanciaKm; 
+									write ('Ingrese el dia del viaje: '); 
+									readln (v[DimL].Dia);
+									write ('Ingrese el dinero transportado: '); 
+									readln (v[DimL].dineroTransportado); 
+							end; 
+							writeln ; 
 		end; 
-end;
+end;  
 
-//--------------------------------------------
+
 {b. Realizar un módulo que reciba el vector generado en a) e informe:
 - El monto promedio transportado de los viajes realizados
 - La distancia recorrida y el día del mes en que se realizó el viaje que transportó menos dinero.
 - La cantidad de viajes realizados cada día del mes.}
-
-// para sacar el monto promedio tenemos que sacar porcentaje: 
-// para sacarlo debemos tener en cuenta el montoPromedio y la DimL que es la cant de viajes
-
-procedure incisoB (v1:vViajes; v2: vDias; DimL: integer; montoPromedio: real ) ; 
+procedure incisoB (v:vector; DimL: integer; var v2:vectorDias); 
 var
-i,Dia_viaje1:  integer; 
-Distancia_viaje1,min1 : real ; 
+Min,montoPromedio,distanciaMinDinero: real; 
+i,diaMin: integer; 
 begin
-min1:= 32000; // ponemos uno de los numeros maximos, asi se va actualizando 
+montoPromedio:= 0 ; 
+diaMin:= 0 ; 
+distanciaMinDinero:= 0 ; 
+Min:= 32000; 
+		for i:= 1 to DimL do 
+			begin
+			montoPromedio:= montoPromedio + v[i].dineroTransportado; 
+				if (v[i].dineroTransportado < Min) then 
+					begin
+						Min:= v[i].dineroTransportado; 
+						diaMin:= v[i].Dia;
+						distanciaMinDinero:= v[i].Km 
+					end; 
+				v2[v[i].Dia] := v2[v[i].Dia] + 1; // Actualizar cantidad de viajes por día
 
-	// calculamos distancia recorrida y dia del mes en el que se realizo el viaje con menos dinero
-	for i:= 1 to DimL do 
-		begin
-					if v1[i].DineroTransportado < min1 then 
-						begin
-							min1:= v1[i].DineroTransportado ; 
-								Dia_viaje1:= v1[i].Dia ; 
-								Distancia_viaje1:= v1[i].km; 
-						end; 
-			
-		end ; 
-
-
-															writeln ('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -') ; 
-						writeln ('El viaje con menos dinero tuvo una distancia de: ',Distancia_viaje1:4:2, ' km, y fue en el dia ',Dia_viaje1 ); 
-								montoPromedio:= (montoPromedio/DimL) ; 		
-										writeln ('El monto promedio transportado de los ',DimL, ' viajes es: $',montoPromedio:4:2); 
-															writeln ('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -') ; 
-	writeln ; 	
-						MostrarVectorDias (v2)
-end; 
-
-
-
-//------------------------------------------- modulo para encontrar la posicion donde esta el km = 100 
-function BuscarDistancia100 (v1: vViajes; DimL: integer): integer; 
-var
-pos: integer; 
-encontre: boolean; 
-begin
-encontre:= false; 
-pos:= 1 ; 
-	while (pos <= DimL) and (encontre = false)  do 
-		begin
-			if v1[pos].Km = 100 then 
-					encontre:= true
-				else
-					pos:= pos + 1 ; 
-	
-		end; 
-		
-	if encontre = false then 
-		BuscarDistancia100:= 0 ; 
-	
-	if encontre = true then 
-		BuscarDistancia100:= pos; 
-end; 
-
-
-
-
-//------------------------------------------------------------
-// este modulo recibe la posicion del viaje con km= 100, y debe borrarlo del vector 
-
-{c. Realizar un módulo que reciba el vector generado en a) y elimine todos los viajes cuya distancia
-recorrida sea igual a 100 km.}
-procedure EliminarDistancia100KM (var v1:vViajes; var DimL: integer ) ; 
-var
-i: integer; 
-pos: integer; 
-begin
-pos:= BuscarDistancia100 (v1,DimL) ; 
-		
-		while (pos >= 1) and (pos<= DimL)   do 
+			end; 
+		writeln ('----> El monto promedio de los viajes realizados es: $',(montoPromedio/DimL):2:2); 
+		writeln ('----> El dia que se transporto menos dinero fue el dia: ',diaMin,  'con una distancia de: ',distanciaMinDinero:2:2, ' Km'); 
+	    writeln ('La cantidad de viajes realizados cada dia del mes son: '); 	
+			for i:= 1 to 31 do 
 				begin
-							for i:= pos to (DimL - 1) do 
-										v1[i]:= v1[i+1];  
-				
-										DimL:= (DimL - 1) ; 
-						pos:= BuscarDistancia100 (v1,DimL) ; 
+					writeln ('En el dia ',i,' se hicieron: ' ,v2[i], ' viajes')  ; 
 				end; 
 end; 
-
+{c. Realizar un módulo que reciba el vector generado en a) y elimine todos los viajes cuya distancia
+recorrida sea igual a 100 km.}
+procedure eliminarViaje100km (var v:vector; var DimL: integer) ; 
+var
+pos,i: integer; 
+ok: boolean; 
+begin
+pos:= 1 ; 
+ok:= false; 
+		while ((pos>= 1) and (pos <= DimL)) do 
+			begin
+					if (v[pos].Km = 100)  then
+						begin
+								for i:= pos to (DimL - 1) do 
+									begin
+										v[i]:= v[i+1] ; 
+									end;
+								DimL:= DimL-1
+						end
+						else
+						pos:= pos + 1 ; 
+			end; 
+end;
 
 var
-viaje: vViajes; 
-dias: vDias; 
-DimL: integer ; 
-montoPromedio: real; 
+v1: vector;
+v2: vectorDias; 
+DimL: integer; 
 begin
-LeerViajes (viaje,dias,DimL,montoPromedio) ; 
-incisoB (viaje,dias,DimL,montoPromedio) ; 
-EliminarDistancia100KM(viaje,DimL)  ; 
-
-	writeln ('El vector con la distancia recorrida en viajes, eliminando los viajes de km = 100, queda con ',DimL, ' viajes ');  
-			// este programa esta completo
-	
-	{estaria bueno mostrar el vector de viajes, con todos los datos, ej: cantidad de viajes del mismo dia (ya está) 
-	* cantidad de kilometros recorridos en total en el mismo dia, cantidad de dinero transportado en el dia
-	* pero bueno, como no lo piden no es necesario}		
-
+inicializarVectorDia(v2); 
+cargarDatos (v1,DimL); 
+eliminarViaje100km(v1,DimL) ; 
+incisoB (v1,DimL,v2); 
 end.
+
+// falta hacer inciso c 
+
